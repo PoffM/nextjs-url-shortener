d7b9002c4163411c84e195ec0705cfcf87d6b859
Mathew Poff <matp@live.ca>
2022-04-20 11:25:39 -0400

    Added a playwright test to shorten a localhost url and follow it.

--- playwright/smoke.test.ts
+++ playwright/smoke.test.ts
@@ -1,15 +1,40 @@
 import { expect, test } from "@playwright/test";
 
-test.setTimeout(35e3);
+test.setTimeout(5_000);
 
-test("go to /", async ({ page }) => {
+test("Go to /", async ({ page }) => {
   await page.goto("/");
 
   // Wait for the "Shorten" button:
   await page.waitForSelector(`text=Shorten`);
 });
 
-test("test 404", async ({ page }) => {
+test("Shorten a URL and follow it", async ({ page }) => {
+  await page.goto("/");
+
+  await page.waitForSelector(`input[name=originalUrl]`);
+  await page.type("input[name=originalUrl]", "localhost:3000/about");
+  await page.click("button >> text=Shorten");
+
+  // Shows the original link:
+  await page.waitForSelector(`text="http://localhost:3000/about"`);
+  // Shows the shortened link:
+  const link = await page.waitForSelector(
+    `a >> text=/http:\\/\\/localhost:3000/.{7}/`
+  );
+
+  // Navigate to the href directly because the link uses target="_blank" which messes up the test navigation.
+  const href = (await link.getAttribute("href")) ?? "/";
+
+  await Promise.all([
+    // Waits for the main frame navigation and returns the main resource response
+    page.waitForNavigation({ url: "http://localhost:3000/about" }),
+    // Triggers the request
+    page.goto(href),
+  ]);
+});
+
+test("Test 404", async ({ page }) => {
   const res = await page.goto("/not-found");
   expect(res?.status()).toBe(404);
 });
---
+++ src/pages/about.tsx
@@ -0,0 +1,4 @@
+/** This is only used as a test page to redirect to in a test. */
+export default function AboutPage() {
+  return <></>;
+}
